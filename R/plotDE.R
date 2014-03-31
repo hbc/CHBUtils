d827d048e7ad18f639a7b9d28fca1b77f2eece7b
John Hutchinson <jnhutchinson@gmail.com>
2014-03-31 14:36:29 -0400

    more fixes to title of plotDE

--- R/plotDE.R
+++ R/plotDE.R
@@ -5,10 +5,10 @@
 #' @param log2foldchange_colid character string, column header of column containing log2 transformed fold change values, required
 #' @param adj_pval_colid character string,column header of column containing adjusted pvalues, required
 #' @param adj_pval_cutoff, optional, defaults to 0.05
-#' @param title character string, title for the plot, optional, defaults to "MA plot"
+#' @param plottitle character string, title for the plot, optional, defaults to "MA plot"
 #' @export
 #' @examples
-#' plotDE(df, basemean_colid="baseMean",log2foldchange_colid=log2FoldChange, adj_pval_colid="padj", adj_pval_cutoff=0.05, title="My MA plot")
+#' plotDE(df, basemean_colid="baseMean",log2foldchange_colid=log2FoldChange, adj_pval_colid="padj", adj_pval_cutoff=0.05, plottitle="My MA plot")
 
 plotDE = function(pvaldf,  basemean_colid=NULL, log2foldchange_colid=NULL, adj_pval_colid=NULL, adj_pval_cutoff=0.05, plottitle="MA plot")  {
   pvaldf$colors <- ifelse(pvaldf[,adj_pval_colid] < adj_pval_cutoff, "sig", "nonsig")
@@ -17,6 +17,6 @@ plotDE = function(pvaldf,  basemean_colid=NULL, log2foldchange_colid=NULL, adj_p
   plot <- ggplot(data = pvaldf, aes(x = log(bm), y = lfc, colour = colors)) + 
     geom_point(size = 3)  + 
     scale_colour_manual(name = "BFH adjusted pvalue", values = c("#00000033", "#FF0000FF"), labels = c(paste("q>", adj_pval_cutoff, sep = ""), paste("q<", adj_pval_cutoff, sep = ""))) + 
-    labs(title = title)
+    labs(title = plottitle)
   plot
 }

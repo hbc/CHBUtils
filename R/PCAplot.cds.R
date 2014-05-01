60ca4c50306ffbdb1ff3a6a4f5a7153509f15f79
John Hutchinson <jnhutchinson@gmail.com>
2014-05-01 17:51:11 -0400

    add secondary categories mapped to shapes back

--- R/PCAplot.cds.R
+++ R/PCAplot.cds.R
@@ -2,6 +2,7 @@
 #'
 #' @param countdataset CountDataSet, required
 #' @param categories character string, column header of column containing different categories to highlight with colors, required
+#' @param categories2 character string, column header of column containing different categories to highlight with shapes, optional
 #' @param title character string, title for the plot, required
 #' @param colorpalette charcter vector of RGB colors (long enough for a color per category), required
 #' @param alpha alpha shading from 0 (transparent) to 1 (opaque), defaults to 1
@@ -12,7 +13,7 @@
 #' @examples
 #' PCAplot.cds(countdataset=cds, categories="condition", title="PCAplot", colorpalette=c("#FF0000", "#00FF00", "#0000FF"), alpha=0.8, numcomponents=4)
 
-PCAplot.cds <- function(countdataset, categories, title, colorpalette, alpha=1, numcomponents=4, normalize.counts=TRUE){
+PCAplot.cds <- function(countdataset, categories, categories2, title, colorpalette, alpha=1, numcomponents=4, normalize.counts=TRUE){
   # check for missing options
   if (missing(countdataset)){
     stop("Function requires an CountDataSet")
@@ -26,8 +27,12 @@ PCAplot.cds <- function(countdataset, categories, title, colorpalette, alpha=1, 
   if (missing(colorpalette)){
     stop("Function requires you to specify a colorpalette")
   }
-  # get metadata
+    # get metadata
   pd <- pData(countdataset)
+  if (!missing(categories2) & length(unique(as.character(unlist(pd[,categories2]))))>5){
+    stop("Only 5 unique types allowed for secondary category")
+  }
+  
   # set colors and alphas
   alpha <- sprintf("%x", ceiling(alpha*255))
   colorpalette <- paste(colorpalette, alpha, sep="")
@@ -38,11 +43,23 @@ PCAplot.cds <- function(countdataset, categories, title, colorpalette, alpha=1, 
   } else {
     df <- counts(countdataset, normalized=FALSE)
   }
+  if(missing(categories2)){
+    shapes <- rep(21, length(categories))
+  } else {
+    numcat2s <- length(unique(as.character(unlist(pd[,categories2])))) 
+    shapes <- c(21:25)[1:numcat2s][factor(as.character(unlist(pd[,categories2])))]
+  }
+  
   # run the actual PCA
   myPca.core <- prcomp(t(df))
   tmpPCAData.core <- as.data.frame(myPca.core$x[,1:numcomponents])
   # plot the result
-  pairs(tmpPCAData.core, bg=colors, col="#606060", cex=2, pch=21, main=title, oma=c(8,5,5,14))
-  legend("right", cex=0.7, col="#606060", pt.bg=colors, pt.cex=1.5, legend=pd[,categories],  pch=21, bty="n", x.intersp=1)
+  if(!missing(categories2)){
+    pairs(tmpPCAData.core, bg=colors, col="#606060", cex=2, pch=shapes, main=title, oma=c(8,5,5,14))
+    legend("right", cex=0.7, col="#606060", pt.bg=colors, pt.cex=1.5, legend=paste(pd[,categories], pd[,categories2], sep="-"),  pch=shapes, bty="n", x.intersp=1)
+  } else {
+    pairs(tmpPCAData.core, bg=colors, col="#606060", cex=2, pch=21, main=title, oma=c(8,5,5,14))
+    legend("right", cex=0.7, col="#606060", pt.bg=colors, pt.cex=1.5, legend=pd[,categories],  pch=21, bty="n", x.intersp=1)
+  }
 }
   

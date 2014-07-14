7392361787d8400133bd4abb4aba4d7615e500ef
John Hutchinson <jnhutchinson@gmail.com>
2014-07-14 14:04:48 -0400

    add col2rownames

--- R/col2rownames.r
+++
@@ -1,6 +0,0 @@
-####################################
-col2rownames <- function(df, colname, removecol=FALSE){
-  row.names(df) <- df[,colname]
-  if(removecol){df[,colname] <- NULL}
-  return(df)
-}

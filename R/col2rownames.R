a1b669efd60bf6acd12f31f1894a20e70a49c672
John Hutchinson <jnhutchinson@gmail.com>
2014-07-14 14:15:01 -0400

    fix example for col2rownames

--- R/col2rownames.R
+++ R/col2rownames.R
@@ -6,7 +6,7 @@
 #' @return dataframe with column contents as rownames
 #' @export
 #' @examples
-#' rownamess2col(dataframe, "newcolumnid")
+#' col2rownames(dataframe, "columnid", FALSE)
 
 
 ####################################

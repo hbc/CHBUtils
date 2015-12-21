34f7ff5a10222b7765d360eb026f9e147fe66569
Lorena <lorena.pantano@gmail.com>
2015-12-21 11:22:35 -0500

    Add docs to different functions to annotate gene ids.

--- R/annotate_df.R
+++ R/annotate_df.R
@@ -10,7 +10,6 @@
 #' @export
 #' @examples
 #' annotate_df(temp, "id", 'mmusculus_gene_ensembl', "ensembl_gene_id", "mgi_symbol")
-
 annotate_df = function(df, df_ensemblid_header, biomart_ensembl_dataset, biomart_ensemblid_filter, biomart_genesymbol_attribute) {
   require(biomaRt)
   ensembl = useMart('ENSEMBL_MART_ENSEMBL', dataset = biomart_ensembl_dataset, host="www.ensembl.org")
@@ -22,23 +21,45 @@ annotate_df = function(df, df_ensemblid_header, biomart_ensembl_dataset, biomart
 }
 
 
-get_biomart_transcript = function(v, type="description")
+#' Get biomart annotation from id
+#'
+#' @param v vector of ids
+#' @param type character name to get from biomart
+#' @param id character defining what kind of id is provided
+#' @param dataset specie database to connect to biomart
+#' @return annotated vector in the same order
+#' @seealso \code{\link{biomaRt}} used to annotate the dataframe
+#' @export
+#' @examples
+#' get_biomrt(v, "gene_biotype")
+get_biomart = function(v, type="description", id="ensembl_gene_id",
+                       dataset="hsapiens_gene_ensembl")
 {
   require(biomaRt)
-  mart = useMart("ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
-  g <- getBM( attributes=c("ensembl_gene_id",type) , filters=
-                "ensembl_gene_id"    , values =as.character(v) ,mart=mart)
-  #row.names(g) = g[,1]
-  out <- c("", length=length(v))
+  v = as.character(v)
+  v[is.na(v)] = "None"
+  mart = useMart("ENSEMBL_MART_ENSEMBL", dataset, host="www.ensembl.org")
+  g <- getBM( attributes=c(id,type) , filters=
+                id, values =as.character(v) ,mart=mart)
+  out <- rep("", length(v))
   idx <- match(g[,1], v)
   out[idx] <- g[,2]
   names(out[idx]) <- g[,1]
   out
-  #g
 }
 
-
-# convertIDs(row.names(.res), "ENSEMBLTRANS", "SYMBOL", org.Mm.eg.db, "useFirst")
+#' Get other Ids for genes in the same order
+#'
+#' @param ids, character vector of gene ids
+#' @param from, type of ID of ids vector
+#' @param to, type of ID to convert
+#' @param db, org.species.eg.db class
+#' @param ifMultiple, what to do when multiple possibles IDs
+#' @return annotated dataframe
+#' @seealso \code{\link{AnnotationDbi::select}} used to annotate the dataframe
+#' @export
+#' @examples
+#' convertIDs(list_genes, "ENSEMBLTRANS", "SYMBOL", org.Mm.eg.db, "useFirst")
 convertIDs <- function( ids, from, to, db, ifMultiple=c("putNA", "useFirst")) {
   stopifnot( inherits( db, "AnnotationDb" ) )
   ifMultiple <- match.arg( ifMultiple )
@@ -52,69 +73,6 @@ convertIDs <- function( ids, from, to, db, ifMultiple=c("putNA", "useFirst")) {
   }
   return( selRes[ match( ids, selRes[,from] ), to ] )
 }
-
-#rewrite plotsplice function
-plotSplice =
-  function (fit, top, coef = ncol(fit),  geneid = NULL, genecolname = NULL,
-            rank = 1L, FDR = 0.05)
-  {
-    if (is.null(genecolname))
-      genecolname <- fit$genecolname
-    else genecolname <- as.character(genecolname)
-    if (is.null(geneid)) {
-      if (rank == 1L)
-        i <- which.min(fit$gene.F.p.value[, coef])
-      else i <- order(fit$gene.F.p.value[, coef])[rank]
-      geneid <- paste(fit$gene.genes[i, genecolname], collapse = ".")
-    }
-    else {
-      geneid <- as.character(geneid)
-      i <- which(fit$gene.genes[, genecolname] == geneid)[1]
-      if (!length(i))
-        stop(paste("geneid", geneid, "not found"))
-    }
-    j <- fit$gene.firstexon[i]:fit$gene.lastexon[i]
-    exoncolname <- fit$exoncolname
-    strcol <- grepl("strand", colnames(fit$gene.genes), ignore.case = TRUE)
-    if (any(strcol))
-      geneid <- paste0(geneid, " (", as.character(fit$gene.genes[i,
-                                                                 strcol])[1], ")")
-    if (is.null(exoncolname)) {
-      plot(fit$coefficients[j, coef], xlab = "Exon", ylab = "logFC (this exon vs rest)",
-           main = geneid, type = "b")
-    }
-    else {
-      exon.id <- fit$genes[j, exoncolname]
-      xlab <- paste("Exon", exoncolname, sep = " ")
-      plot(fit$coefficients[j, coef], xlab = "", ylab = "logFC (this exon vs rest)",
-           main = geneid, type = "b", xaxt = "n", cex=1)
-      axis(1, at = 1:length(j), labels = exon.id, las = 2,
-           cex.axis = 1)
-      # mtext(xlab, side = 1, padj = 5.2)
-      # top <- topSplice(fit, coef = coef, number = Inf, test = "t",
-      #                  FDR = FDR)
-
-      m <- which(top[, genecolname] %in% as.character(fit$gene.genes[i,genecolname]))
-      mark = NULL
-      if (length(m) > 0) {
-        if (length(m) == 1)
-          cex <- 1.5
-        else {
-          abs.fdr <- abs(log10(top$FDR[m]))
-          from <- range(abs.fdr)
-          to <- c(1, 2)
-          cex <- (abs.fdr - from[1])/diff(from) * diff(to) +
-            to[1]
-        }
-        mark <- match(top[m, exoncolname], exon.id)
-        points((1:length(j))[mark], fit$coefficients[j[mark],
-                                                     coef], col = "red", pch = 16, cex = cex)
-      }
-    }
-    abline(h = 0, lty = 2)
-    invisible()
-    mark
-  }
 
 # reduce clusterprofile output
 reduce_cp = function(genes, lim=100){
